51293c33205f579dca95417fa7c07db741a6e42d
Will Murdoch <wmurdoch@brownsteingroup.com>
2019-11-08 13:15:52 -0500

    Progress:

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var spin = require('./routes/spin');
+var play = require('./routes/play');
 
 var app = express();
 var server = require('http').Server(app);
@@ -29,7 +30,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/spin', spin);
+app.use('/play', play);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/spin.js
+++ routes/spin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.io.emit("socketToMe", "users");
+  res.io.emit("spin", "spin");
   res.send('respond with a resource.');
 });
 
